85639ee9e583352a306cae539af9da2d5fd7cb30
Ricardo Del Rio Fuente <ricardo.delriofuente@metaswitch.com>
2022-10-24 18:18:14 +0100

    implement tom's code review comments

--- src/day03.rs
+++ src/day03.rs
@@ -1,38 +1,32 @@
 use std::str::Lines;
 
-fn trees_in_slope(right_offset: usize, down_offset: usize, lines_iterator: Lines<'_>) -> usize {
-    let mut cyclic_position = right_offset;
+fn trees_in_slope(right_offset: usize, down_offset: usize, input_lines: &str) -> usize {
+    let mut cyclic_position = 0;
     let mut trees_in_los = 0;
-    let mut next_line_offset = 0;
 
-    for line in lines_iterator {
-        if next_line_offset == down_offset {
-            // Cyclical mountain => use modulus operator to wrap-around each line
-            cyclic_position %= line.len();
+    let lines_iterator: Lines = input_lines.lines();
 
-            if line.chars().nth(cyclic_position).unwrap() == '#' {
-                trees_in_los += 1;
-            };
+    for line in lines_iterator.step_by(down_offset) {
+        // Cyclical mountain => use modulus operator to wrap-around each line
+        cyclic_position %= line.len();
 
-            cyclic_position += right_offset;
-            next_line_offset = 0;
-        }
+        if line.chars().nth(cyclic_position).unwrap() == '#' {
+            trees_in_los += 1;
+        };
 
-        next_line_offset += 1;
+        cyclic_position += right_offset;
     }
     trees_in_los
 }
 
 pub fn day03(input_lines: &str) -> (String, String) {
-    let lines_iterator: Lines = input_lines.lines();
+    let answer1 = trees_in_slope(3, 1, input_lines);
 
-    let answer1 = trees_in_slope(3, 1, lines_iterator.clone());
-
-    let trees_r1_d1 = trees_in_slope(1, 1, lines_iterator.clone());
+    let trees_r1_d1 = trees_in_slope(1, 1, input_lines);
     let trees_r3_d1 = answer1;
-    let trees_r5_d1 = trees_in_slope(5, 1, lines_iterator.clone());
-    let trees_r7_d1 = trees_in_slope(7, 1, lines_iterator.clone());
-    let trees_r1_d2 = trees_in_slope(1, 2, lines_iterator);
+    let trees_r5_d1 = trees_in_slope(5, 1, input_lines);
+    let trees_r7_d1 = trees_in_slope(7, 1, input_lines);
+    let trees_r1_d2 = trees_in_slope(1, 2, input_lines);
 
     let answer2 = trees_r1_d1 * trees_r3_d1 * trees_r5_d1 * trees_r7_d1 * trees_r1_d2;
 
