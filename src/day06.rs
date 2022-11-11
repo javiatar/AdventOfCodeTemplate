83ee40810248f9d7a16f16c4289c86699ea33139
Ricardo Del Rio Fuente <ricardo.delriofuente@metaswitch.com>
2022-11-11 10:13:25 +0000

    Day 6 passing

--- src/day06.rs
+++ src/day06.rs
@@ -1,7 +1,36 @@
+use itertools::Itertools;
+use std::collections::HashSet;
+
+fn get_num_unique_qs_in_group(ppl_answers: &str) -> usize {
+    // Want num uniquely appearing letters in entire group (ignore newlines b/w ppl)
+    ppl_answers.chars().filter(|&c| c != '\n').unique().count()
+}
+
+fn get_num_common_qs(ppl_answers: &str) -> usize {
+    let mut sets: Vec<HashSet<char>> = Vec::new();
+
+    // Convert group answers to vector of HashSets of each person's answers
+    ppl_answers.split('\n').for_each(|ans| {
+        sets.push(ans.chars().collect::<HashSet<char>>());
+    });
+
+    let mut iter = sets.into_iter();
+
+    let first = iter.next();
+
+    let intersection = first.map(|set| iter.fold(set, |set1, set2| &set1 & &set2));
+
+    intersection.unwrap_or_default().len()
+}
+
 pub fn day06(input_lines: &str) -> (String, String) {
-    let _ = input_lines;
-    let answer1 = 0;
-    let answer2 = 0;
+    // Sum unique qs for each group (split by \n\n)
+    let answer1: usize = input_lines
+        .split("\n\n")
+        .map(get_num_unique_qs_in_group)
+        .sum();
+
+    let answer2: usize = input_lines.split("\n\n").map(get_num_common_qs).sum();
     (format!("{}", answer1), format!("{}", answer2))
 }
 
@@ -11,12 +40,52 @@ mod tests {
 
     #[test]
     fn check_day06_part1_case1() {
-        assert_eq!(day06("").0, "0".to_string())
+        assert_eq!(
+            day06(
+                "abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b"
+            )
+            .0,
+            "11".to_string()
+        )
     }
 
     #[test]
     fn check_day06_part2_case1() {
-        assert_eq!(day06("").1, "0".to_string())
+        assert_eq!(
+            day06(
+                "abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b"
+            )
+            .1,
+            "6".to_string()
+        )
     }
 
     #[test]
